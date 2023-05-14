f48507eb9d9971e324fdc85a6aab504e5bcd654c
zoeyvarnax <gksfkdus@gamil.com>
2023-05-15 00:47:04 +0900

    FEAT: 여행 계획 업데이트 기능 추가
    여행 계획 업데이트 요청 시 계획의 제목/시작일/종료일 및 세부 장소의 날짜/순서 수정 가능하도록 수정.
    
    close #11

--- src/main/java/com/ssafy/plan/model/service/PlanService.java
+++ src/main/java/com/ssafy/plan/model/service/PlanService.java
@@ -7,6 +7,7 @@ import com.ssafy.plan.model.PlanDto;
 public interface PlanService {
 	Long addPlan(PlanDto planDto) throws Exception;
 	void deletePlan(Long id) throws Exception;
+	void updatePlan(PlanDto planDto) throws Exception;
 	PlanDto findPlanById(Long id) throws Exception;
 	List<PlanDto> findPlansByMemberId(String memberId) throws Exception;
 	List<PlanDto> findAll() throws Exception;
